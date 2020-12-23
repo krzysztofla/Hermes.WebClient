f654f645e19178970a8f0cec7ad4b4a94d4931a2
krzysztofla <krzysztof.lach@icloud.com>
2020-12-23 16:40:49 +0100

    Added loading screen and blazorise ant design libs

--- src/Program.cs
+++ src/Program.cs
@@ -1,15 +1,14 @@
 using System;
 using System.Net.Http;
-using System.Collections.Generic;
 using System.Threading.Tasks;
-using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Authorization;
 using Hermes.WebClient.Services.Auth;
+using Blazorise;
+using Blazorise.AntDesign;
+using Blazorise.Icons.FontAwesome;
 
 namespace Hermes.WebClient
 {
@@ -18,18 +17,29 @@ namespace Hermes.WebClient
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            
+
             builder.Services.AddBlazoredLocalStorage();
             builder.Services.AddAuthorizationCore();
 
+            builder.Services
+                    .AddBlazorise(options =>
+                    {
+                        options.ChangeTextOnKeyPress = true;
+                    })
+                    .AddAntDesignProviders()
+                    .AddFontAwesomeIcons();
+
             builder.Services.AddScoped<AuthenticationStateProvider, ApiAuthenticationStateProvider>();
             builder.Services.AddScoped<IAuthService, AuthService>();
-            
+
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-            await builder.Build().RunAsync();
+            var host = builder.Build();
+            host.Services
+              .UseAntDesignProviders()
+              .UseFontAwesomeIcons();
+            await host.RunAsync();
         }
     }
 }
